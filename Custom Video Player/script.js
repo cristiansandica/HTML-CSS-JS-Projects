e996181646d2f3ba8b1ba7c65ca32eb06bf3ef59
Sandica Cristian <cristian.sandica17@gmail.com>
2020-06-03 05:47:58 +0300

    update jsproj

--- Custom Video Player/script.js
+++ Custom Video Player/script.js
@@ -4,59 +4,53 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-
 // Play & pause video
 function toggleVideoStatus() {
-    if (video.paused) {
-        video.play();
-    }
-    else {
-        video.pause()
-    }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // update play/pause icon
 function updatePlayIcon() {
-    if (video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-    }
-    else {
-        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-    }
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-    video.currentTime = (+progress.value * video.duration) / 100;
-
-    // Get minutes
-    let mins = Math.floor(video.currentTime / 60);
-    if (mins < 10) {
-        mins = '0' + String(mins);
-    }
-
-    // Get seconds
-    let secs = Math.floor(video.currentTime % 60);
-    if (secs < 10) {
-        secs = '0' + String(secs);
-    }
-
-    timestamp.innerHTML = `${mins}:${secs}`;
-
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
 function stopVideo() {
-    video.currentTime = 0;
-    video.pause();
+  video.currentTime = 0;
+  video.pause();
 }
-
-
-// update progress & timestamp
-function updateProgress() {
-    progress.value = (video.currentTime / video.duration) * 100;
-}
-
 
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
--- Exchange Rate/script.js
+++ Exchange Rate/script.js
@@ -6,43 +6,43 @@
 // }
 
 
-const currencyEl_one = document.getElementById("currency-one");
-const currencyEl_two = document.getElementById("currency-two");
-
-const amountEl_one = document.getElementById("amount-one");
-const amountEl_two = document.getElementById("amount-two");
-
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
 
+
 // Fetch exchange rates and update the DOM
-function calculate(){
-      const currency_one = currencyEl_one.value;
-      const currency_two = currencyEl_two.value;
+function caclulate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
 
-      fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-      .then(res => res.json())
-      .then(data => {
-          const rate = data.rates[currencyEl_two];
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
 
-          rateEl.innerText = `1 ${currency_one} = ${rate} ${currencyEl_two}`;
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-          amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-      })
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
 // Event listeners
-currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
 
 swap.addEventListener('click', () => {
-    const temp = currencyEl_one.value;
-    currencyEl_one.value = currencyEl_two.value;
-    currencyEl_two.value = temp;
-    calculate();
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  caclulate();
 });
 
-calculate();
+caclulate();
